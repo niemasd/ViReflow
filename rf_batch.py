ebe46b16a7e290900155edad9f6771600f337588
Niema Moshiri <niemamoshiri@gmail.com>
2021-08-05 17:23:44 -0700

    Removed execution function

--- rf_batch.py
+++ rf_batch.py
@@ -2,10 +2,9 @@
 '''
 Create a Reflow run file that batch-executes many other Reflow run files.
 '''
-from os import environ, makedirs, remove
-from os.path import abspath, expanduser, isfile
-from subprocess import Popen, PIPE
-from sys import platform, stderr, stdout
+from os import remove
+from os.path import abspath, isfile
+from sys import stderr, stdout
 import argparse
 import sys
 
@@ -16,14 +15,12 @@ REFLOW_EXEC_MAC_URL = 'https://github.com/grailbio/reflow/releases/download/refl
 
 # help text
 HELP_TEXT_ABSPATH = "Use absolute paths to Reflow run files"
-HELP_TEXT_RUN = "Run batch RF file using 'reflow run'"
 
 # parse user args
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output Batch Reflow Run File (RF)")
     parser.add_argument('-a', '--abs_path', action="store_true", help=HELP_TEXT_ABSPATH)
-    parser.add_argument('-r', '--run', action="store_true", help=HELP_TEXT_RUN)
     parser.add_argument('rf_files', metavar='RF', type=str, nargs='+', help="Input Reflow Run Files (RF)")
     args = parser.parse_args()
     for rf in args.rf_files:
@@ -72,7 +69,7 @@ def run_gui():
 
         # create applet
         root = Tk()
-        root.geometry("600x450")
+        root.geometry("600x400")
 
         # set up main frame
         frame = Frame(root)
@@ -93,29 +90,6 @@ def run_gui():
                 button_out.configure(text="%s%s" % (button_out_prefix,fn))
         button_out = Button(frame, text="%s%s" % (button_out_prefix,button_out_nofile), command=find_filename_out)
         button_out.pack(padx=3, pady=3)
-
-        # handle run toggle
-        check_run_var = IntVar(frame)
-        check_run = Checkbutton(frame, text=HELP_TEXT_RUN, variable=check_run_var, onvalue=1, offvalue=0)
-        check_run.pack()
-
-        # handle AWS_ACCESS_KEY_ID
-        entry_AWS_ACCESS_KEY_ID_default = "Enter AWS_ACCESS_KEY_ID (only necessary for 'reflow run')"
-        entry_AWS_ACCESS_KEY_ID = Entry(frame, width=60)
-        entry_AWS_ACCESS_KEY_ID.insert(END, entry_AWS_ACCESS_KEY_ID_default)
-        entry_AWS_ACCESS_KEY_ID.pack()
-
-        # handle AWS_SECRET_ACCESS_KEY
-        entry_AWS_SECRET_ACCESS_KEY_default = "Enter AWS_SECRET_ACCESS_KEY (only necessary for 'reflow run')"
-        entry_AWS_SECRET_ACCESS_KEY = Entry(frame, width=60)
-        entry_AWS_SECRET_ACCESS_KEY.insert(END, entry_AWS_SECRET_ACCESS_KEY_default)
-        entry_AWS_SECRET_ACCESS_KEY.pack()
-
-        # handle AWS_REGION
-        entry_AWS_REGION_default = "Enter AWS_REGION (only necessary for 'reflow run')"
-        entry_AWS_REGION = Entry(frame, width=60)
-        entry_AWS_REGION.insert(END, entry_AWS_REGION_default)
-        entry_AWS_REGION.pack()
 
         # handle input RF file selection
         def find_filenames_rfs():
@@ -154,15 +128,7 @@ def run_gui():
                 sys.argv.append('-o'); sys.argv.append(out_fn)
                 if isfile(out_fn):
                     remove(out_fn)
-                if check_run_var.get() == 1:
-                    sys.argv.append('--run')
                 sys.argv += [rf.strip() for rf in text_rfs.get('1.0', END).strip().splitlines()]
-                if entry_AWS_ACCESS_KEY_ID.get().strip() != entry_AWS_ACCESS_KEY_ID_default.strip():
-                    environ['AWS_ACCESS_KEY_ID'] = entry_AWS_ACCESS_KEY_ID.get().strip()
-                if entry_AWS_SECRET_ACCESS_KEY.get().strip() != entry_AWS_SECRET_ACCESS_KEY_default.strip():
-                    environ['AWS_SECRET_ACCESS_KEY'] = entry_AWS_SECRET_ACCESS_KEY.get().strip()
-                if entry_AWS_REGION.get().strip() != entry_AWS_REGION_default.strip():
-                    environ['AWS_REGION'] = AWS_REGION.get().strip()
                 try:
                     root.destroy()
                 except:
@@ -178,66 +144,8 @@ def run_gui():
     if len(sys.argv) == 1:
         exit()
 
-# run reflow as well
-def run_reflow(batch_rf_fn):
-    def execute(batch_rf_fn):
-        # check if reflow is in PATH
-        reflow_exe_path = None
-        try:
-            o,e = Popen(['reflow', 'run', '-h'], stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
-            if e.decode().startswith('usage: reflow run'):
-                reflow_exe_path = 'reflow'
-        except:
-            pass
-
-        # if reflow doesn't exist, set it up
-        if reflow_exe_path is None:
-            if GUI:
-                text_stdout.insert(END, "Setting up reflow...\n")
-            else:
-                print("Setting up reflow...", flush=True)
-            if 'linux' in platform:
-                url = REFLOW_EXEC_LINUX_URL
-            elif 'darwin' in platform:
-                url = REFLOW_EXEC_MAC_URL
-            else:
-                if GUI:
-                    text_stdout.insert(END, "Your platform is not currently supported: %s\n" % platform)
-                else:
-                    print("Your platform is not currently supported: %s" % platform, flush=True)
-            from urllib.request import urlopen
-            reflow_dir = expanduser('~/.reflow')
-            reflow_exe_path = '%s/reflow' % reflow_dir
-            makedirs(reflow_dir, exist_ok=True)
-            reflow_exe = urlopen(url).read()
-            f = open(reflow_exe_path, 'wb'); f.write(reflow_exe); f.close()
-
-        # run reflow
-        command = [reflow_exe_path, 'run', batch_rf_fn]
-        with Popen(command, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as p:
-            for line in p.stderr:
-                if GUI:
-                    text_stderr.insert(END, line)
-                else:
-                    print(line, end='', flush=True)
-        if GUI:
-            root.destroy()
-
-    if GUI and False: # TODO REMOVE and False
-        root = TK()
-        root.geometry("600x400")
-        frame = Frame(root)
-        header = Label(frame, text="Reflow Console", font=('Arial',24))
-        header.pack()
-        text_stdout = ScrolledText(frame, height=40)
-        text_stdout.pack()
-    else:
-        execute(batch_rf_fn)
-
 # main execution
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        GUI = True; run_gui()
+        run_gui()
     args = main()
-    if args.run:
-        run_reflow(args.output.name)
