f8731e36d1480aa8a132419ee4e6ba04ff4bfe32
Niema Moshiri <niemamoshiri@gmail.com>
2022-02-08 18:36:09 -0800

    Incremented version

--- ViReflow.py
+++ ViReflow.py
@@ -14,7 +14,7 @@ import argparse
 import sys
 
 # useful constants
-VERSION = 'latest' #'1.0.16' TODO
+VERSION = '1.0.16'
 RELEASES_URL = 'https://api.github.com/repos/niemasd/ViReflow/tags'
 RUN_ID_ALPHABET = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.')
 READ_TRIMMERS = {
